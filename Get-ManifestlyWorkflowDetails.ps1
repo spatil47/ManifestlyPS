ff8ebc587fe04953a3be7c1c13c2c3f1af7f7d5a
Siddharth Patil <sid@sidpatil.com>
2020-05-09 14:24:48 -0400

    Added help for "get workflow details"

--- Get-ManifestlyWorkflowDetails.ps1
+++ Get-ManifestlyWorkflowDetails.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+    List details of a Manifestly workflow.
+
+.DESCRIPTION
+    This script uses the Manifestly API to list the details of a workflow.
+
+.PARAMETER Server
+    The server providing the Manifestly API.
+
+.PARAMETER APIKey
+    The Manifestly API key associated with the department of the workflow.
+
+.PARAMETER WorkflowID
+    The ID of the workflow.
+
+.INPUTS
+    None.
+
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+        User's account ID, workflow's business days, channel, description, expected duration, units of expected duration, external ID, whether steps are hidden from external users, workflow ID, steps, tag list, and workflow title.
+
+.LINK
+    https://manifestlyapi.docs.apiary.io/#reference/0/workflows-collection/list-all-workflows
+#>
+
+#Requires -Version 4.0
 Param(
     [Parameter()]
         [string] $Server = "https://api.manifest.ly",
