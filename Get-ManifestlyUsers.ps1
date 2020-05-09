74681949dc3326cdedfa223db96d1cf28d395740
Siddharth Patil <sid@sidpatil.com>
2020-05-09 14:06:29 -0400

    Added help for "list all users"

--- Get-ManifestlyUsers.ps1
+++ Get-ManifestlyUsers.ps1
@@ -1,3 +1,29 @@
+<#
+.SYNOPSIS
+    List all Manifestly users in a department.
+
+.DESCRIPTION
+    This script uses the Manifestly API to list all users in a department.
+    The department is specified by the API key.
+
+.PARAMETER Server
+    The server providing the Manifestly API.
+
+.PARAMETER APIKey
+    The Manifestly API key associated with the department of the users to be retrieved.
+
+.INPUTS
+    None.
+
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+        One object per user, containing ID, membership ID, full name, role, display name, and username.
+
+.LINK
+    https://manifestlyapi.docs.apiary.io/#reference/0/users-collection/list-all-usersc
+#>
+
+#Requires -Version 4.0
 Param(
     [Parameter()]
         [string] $Server = "https://api.manifest.ly",
