d1cd7de674539e09d3ca5be9ad6cc0a10854e66f
Siddharth Patil <sid@sidpatil.com>
2020-05-09 10:32:25 -0400

    Initial implementation of "get checklist run step comments"

---
+++ Get-ManifestlyChecklistRunStepComments.ps1
@@ -0,0 +1,22 @@
+Param(
+    [Parameter()]
+        [string] $Server = "https://api.manifest.ly",
+    [Parameter(mandatory=$true)]
+        [string] $APIKey,
+    [Parameter(mandatory=$true)]
+        [string] $RunID,
+        [Parameter(mandatory=$true)]
+        [string] $RunStepID
+)
+
+$Query = @{
+    api_key = $APIKey
+}
+
+$Request = @{
+    Uri = "$Server/api/v1/runs/$RunID/run_steps/$RunStepID/comments"
+    Body = $query
+    Method = "Get"
+}
+
+(Invoke-RestMethod @Request).comments
