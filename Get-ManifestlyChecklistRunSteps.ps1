2a94ab943647c81aa94ec635e5ae61b801d6f915
Siddharth Patil <sid@sidpatil.com>
2020-06-14 19:23:34 -0400

    Added documentation for "get checklist run steps"

--- Get-ManifestlyChecklistRunSteps.ps1
+++ Get-ManifestlyChecklistRunSteps.ps1
@@ -1,3 +1,32 @@
+<#
+.SYNOPSIS
+    Get steps of a Manifestly checklist run.
+
+.DESCRIPTION
+    This script uses the Manifestly API to get steps of a specified checklist run for a specified department.
+    The department is specified by the API key.
+
+.PARAMETER Server
+    The server providing the Manifestly API.
+
+.PARAMETER APIKey
+    The Manifestly API key associated with the department of the checklist run details to be retrieved.
+
+.PARAMETER APIKey
+    The ID of the checklist run.
+
+.INPUTS
+    None.
+
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+        An object containing properties corresponding to the steps of the checklist run.
+
+.LINK
+    https://manifestlyapi.docs.apiary.io/#reference/0/checklist-run-steps-collection/list-all-steps-for-a-checklist-run
+#>
+
+#Requires -Version 4.0
 Param(
     [Parameter()]
         [string] $Server = "https://api.manifest.ly",
