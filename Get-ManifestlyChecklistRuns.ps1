3a712f3e948890eff1816455d1ac8675f7ad7782
Siddharth Patil <sid@sidpatil.com>
2020-06-19 10:53:05 -0400

    Added documentation for "get checklist runs"

--- Get-ManifestlyChecklistRuns.ps1
+++ Get-ManifestlyChecklistRuns.ps1
@@ -1,3 +1,50 @@
+<#
+.SYNOPSIS
+    Get Manifestly checklist runs.
+
+.DESCRIPTION
+    This script uses the Manifestly API to retrieve specified checklist runs for a specified department.
+    The department is specified by the API key.
+
+.PARAMETER Server
+    The server providing the Manifestly API.
+
+.PARAMETER APIKey
+    The Manifestly API key associated with the department of the checklist runs to be retrieved.
+
+.PARAMETER WorkflowID
+    The ID of the workflow of the checklist runs to be retrieved.
+
+.PARAMETER RunID
+    The ID of he checklist runs to be retrieved.
+
+.PARAMETER RunExternalID
+    The external ID of the checklist runs to be retrieved.
+
+.PARAMETER Title
+    The title of the checklist runs to be retrieved.
+
+.PARAMETER Status
+    The status of the checklist runs to be retrieved.
+
+.PARAMETER RecordsPerPage
+    The number of records to return per request.
+
+.PARAMETER Page
+    The page to be returned.
+
+.INPUTS
+    None.
+
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+        An array of objects, each containing properties corresponding to the checklist run.
+
+.LINK
+    https://manifestlyapi.docs.apiary.io/#reference/0/checklist-runs-collection/list-all-checklist-runs
+#>
+
+#Requires -Version 4.0
 Param(
     [Parameter()]
         [string] $Server = "https://api.manifest.ly",
@@ -12,7 +59,7 @@ Param(
     [ValidateSet("started", "late", "completed")]
         [string] $Status,
     [Parameter()]
-        [string] $RecordsPerPage,
+        [string] $RecordsPerPage = 30,
     [Parameter()]
         [string] $Page
 )
