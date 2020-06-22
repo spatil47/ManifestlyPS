9de5e370f92a603cbf0709e67b539aab1cc3487d
Siddharth Patil <sid@sidpatil.com>
2020-06-22 14:21:28 -0400

    Added documentation for "get workflow steps"

--- Get-ManifestlyWorkflowSteps.ps1
+++ Get-ManifestlyWorkflowSteps.ps1
@@ -1,3 +1,32 @@
+<#
+.SYNOPSIS
+    Get steps of a Manifestly workflow.
+
+.DESCRIPTION
+    This script uses the Manifestly API to get steps of a specified workflow for a specified department.
+    The department is specified by the API key.
+
+.PARAMETER Server
+    The server providing the Manifestly API.
+
+.PARAMETER APIKey
+    The Manifestly API key associated with the department of the workflow steps to be retrieved.
+
+.PARAMETER WorkflowID
+    The ID of the workflow.
+
+.INPUTS
+    None.
+
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+        An array of objects containing properties corresponding to the steps of the workflow.
+
+.LINK
+    https://manifestlyapi.docs.apiary.io/#reference/0/workflow-steps-collection/list-all-workflow-steps
+#>
+
+#Requires -Version 4.0
 Param(
     [Parameter()]
         [string] $Server = "https://api.manifest.ly",
