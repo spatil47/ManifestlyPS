d796a810d4d134d3c6c48ec65c53344d3e9be872
Siddharth Patil <sid@sidpatil.com>
2020-06-22 14:29:03 -0400

    Added documentation for "get workflow hooks"

--- Get-ManifestlyWorkflowHooks.ps1
+++ Get-ManifestlyWorkflowHooks.ps1
@@ -1,3 +1,32 @@
+<#
+.SYNOPSIS
+    Get hooks of a Manifestly workflow.
+
+.DESCRIPTION
+    This script uses the Manifestly API to get hooks of a specified workflow for a specified department.
+    The department is specified by the API key.
+
+.PARAMETER Server
+    The server providing the Manifestly API.
+
+.PARAMETER APIKey
+    The Manifestly API key associated with the department of the workflow to be retrieved.
+
+.PARAMETER WorkflowID
+    The ID of the workflow.
+
+.INPUTS
+    None.
+
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+        An array of objects containing properties corresponding to the hooks of the workflow.
+
+.LINK
+    https://manifestlyapi.docs.apiary.io/#reference/0/workflow-steps-hooks/list-hooks
+#>
+
+#Requires -Version 4.0
 Param(
     [Parameter()]
         [string] $Server = "https://api.manifest.ly",
