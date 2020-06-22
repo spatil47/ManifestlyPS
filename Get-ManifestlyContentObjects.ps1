ca5eff882ff92e5c611b32eca52278a4329bee25
Siddharth Patil <sid@sidpatil.com>
2020-06-22 13:37:05 -0400

    Added documentation for "get content objects"

--- Get-ManifestlyContentObjects.ps1
+++ Get-ManifestlyContentObjects.ps1
@@ -1,3 +1,35 @@
+<#
+.SYNOPSIS
+    Get Manifestly content objects.
+
+.DESCRIPTION
+    This script uses the Manifestly API to retrieve contents for a specified workflow step for a specified department.
+    The department is specified by the API key.
+
+.PARAMETER Server
+    The server providing the Manifestly API.
+
+.PARAMETER APIKey
+    The Manifestly API key associated with the department of the workflow.
+
+.PARAMETER WorkflowID
+    The ID of the workflow of the content objects to be retrieved.
+
+.PARAMETER WorkflowStepID
+    The ID of the workflow step.
+
+.INPUTS
+    None.
+
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+        An array of objects, each containing properties corresponding to the content objects.
+
+.LINK
+    https://manifestlyapi.docs.apiary.io/#reference/0/content-objects-collection/list-content-objects
+#>
+
+#Requires -Version 4.0
 Param(
     [Parameter()]
         [string] $Server = "https://api.manifest.ly",
