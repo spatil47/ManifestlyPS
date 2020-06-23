6371beb03b16bed3b1ea8b76edfc7406ebf11aa9
Siddharth Patil <sid@sidpatil.com>
2020-06-23 10:49:05 -0400

    Added utility for generating HTTP URL with query parameters

---
+++ New-HttpUrl.ps1
@@ -0,0 +1,17 @@
+﻿Param(
+    [Parameter(mandatory=$true)]
+        [string] $Server,
+    [Parameter(mandatory=$true)]
+        [hashtable] $QueryParameters
+)
+
+$QueryString = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
+ 
+$QueryParameters.GetEnumerator() | ForEach-Object {
+    $QueryString.Add($_.Name, $_.Value)
+}
+ 
+$Url = [System.UriBuilder] $Server
+$Url.Query = $QueryString.ToString()
+ 
+return $Url.Uri.OriginalString
